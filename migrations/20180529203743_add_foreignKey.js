6da0609e8dd6542f639878f10780523aa559f249
Jeff <jeff.tang@live.com>
2018-05-29 16:50:01 -0400

    added migration to attach foreign key

---
+++ migrations/20180529203743_add_foreignKey.js
@@ -0,0 +1,12 @@
+exports.up = function (knex, Promise) {
+  return knex.schema.alterTable('milestones', table => {
+    table.integer('famous_person_id').unsigned()
+    table.foreign('famous_person_id').references('famous_people.id')
+  })
+};
+
+exports.down = function (knex, Promise) {
+  return knex.schema.alterTable('milestones', table => {
+    table.dropColumn('famous_person_id')
+  })
+};
